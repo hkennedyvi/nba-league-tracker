4a46837de37cc2a2f2428e013776791a98b6fc56
atcanbolat <kangalcanbolat@gmail.com>
2020-02-15 16:36:35 -0800

    revert back

--- models/index.js
+++ models/index.js
@@ -4,8 +4,8 @@ var fs = require("fs");
 var path = require("path");
 var Sequelize = require("sequelize");
 var basename = path.basename(module.filename);
-var env = process.env.NODE_ENV || "production";
-var config = require(__dirname + "/../config/config.js")[env];
+var env = process.env.NODE_ENV || "development";
+var config = require(__dirname + "/../config/config.json")[env];
 var db = {};
 
 if (config.use_env_variable) {
